53d72910c84bf5f2b0e02220a509c1bd91860e45
AHFahim009 <ahfba009@gmail.com>
2024-11-24 17:26:34 +0600

    add buyer profile service logic

--- src/modules/buyerProfile/buyerProfile.routes.ts
+++ src/modules/buyerProfile/buyerProfile.routes.ts
@@ -1,8 +1,20 @@
 import { Router } from "express";
-import { BuyerControllers } from "./buyerProfile.controllers";
+import { BuyerProfileControllers } from "./buyerProfile.controllers";
 
 const router = Router()
 
-router.post("/create-buyer", BuyerControllers.createBuyer)
+router.post("/create-buyer", BuyerProfileControllers.createBuyer)
+
+// get single buyer
+router.get("/:buyerId", BuyerProfileControllers.getSingleBuyerProfile)
+
+// get all buyer
+router.get("/", BuyerProfileControllers.getAllBuyerProfile)
+
+// update buyer
+router.put("/:buyerId", BuyerProfileControllers.updateBuyerProfile)
+
+// delete buyer
+router.delete("/:buyerId", BuyerProfileControllers.deleteBuyerProfile)
 
 export const BuyerProfileRoutes = router
--- src/modules/buyerProfile/buyerProfile.services.ts
+++ src/modules/buyerProfile/buyerProfile.services.ts
@@ -1,9 +1,40 @@
+import { AppError } from "../../middleware/globalErrorHanlde"
+import { IBuyerProfile } from "./buyerProfile.interface"
+import BuyerProfileModel from "./buyerProfile.models"
 
-const createBuyer = async () => {
+const createBuyer = async (payload: IBuyerProfile) => {
+    const { userId } = payload
+    const isBuyerProfileExit = await BuyerProfileModel.findById(userId)
+    if (isBuyerProfileExit) throw new AppError("Buyer profile already exit", 401)
+    const result = new BuyerProfileModel(payload)
+    return await result.save()
 
 
 }
+const getSingleBuyerProfile = async (id: string) => {
+    const buyerProfile = await BuyerProfileModel.findOne({ _id: id, status: "active" });
+    if (!buyerProfile) throw new AppError("Buyer profile not found", 404);
+    return buyerProfile;
+};
 
-export const BuyerServices = {
-    createBuyer
+const getAllBuyerProfile = async () => {
+    return await BuyerProfileModel.find({ status: "active" });
+};
+
+const updateBuyerProfile = async (id: string, updateData: Partial<IBuyerProfile>) => {
+    const updatedProfile = await BuyerProfileModel.findByIdAndUpdate(id, updateData, { new: true });
+    if (!updatedProfile) throw new AppError("Buyer profile not found", 404);
+    return updatedProfile;
+};
+
+const deleteBuyerProfile = async (id: string) => {
+    const deletedProfile = await BuyerProfileModel.findByIdAndUpdate(id, { status: "deleted" }, { new: true });
+    if (!deletedProfile) throw new AppError("Buyer profile not found", 404);
+    return deletedProfile;
+};
+
+
+
+export const BuyerProfileServices = {
+    createBuyer, updateBuyerProfile, getSingleBuyerProfile, getAllBuyerProfile, deleteBuyerProfile
 }
